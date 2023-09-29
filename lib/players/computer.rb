951e3e99d2dca367be32bbcf7ca1f45e7c444390
AbdEl-Rahman21 <abdel292004@gmail.com>
2023-09-29 22:47:58 +0300

    Add computer AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,4 +4,35 @@ require_relative './player'
 
 # Class for the computer player
 class Computer < Player
+  def initialize(color)
+    super(color)
+
+    @chosen_piece = 0
+  end
+
+  def get_choice(step)
+    if step == 1
+      select_piece
+    else
+      select_move
+    end
+  end
+
+  def select_piece
+    @chosen_piece = @pieces.filter { |piece| !piece.moves.empty? }.sample
+
+    [(@chosen_piece.coordinates[0] + 9).to_s(36), @chosen_piece.coordinates[1].to_s].join
+  end
+
+  def select_move
+    move = @chosen_piece.moves.sample
+
+    [(move[0] + 9).to_s(36), move[1].to_s].join
+  end
+
+  def promote(piece)
+    @pieces.unshift(Queen.new(piece.coordinates, @color))
+
+    @pieces.delete(piece)
+  end
 end
